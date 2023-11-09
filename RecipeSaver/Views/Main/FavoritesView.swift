40bffe7c55ee40e883c61f2fb51256b3996db383
jkwon-dev <kwonejk@gmail.com>
2023-11-10 01:10:19 +0800

    Project setup

---
+++ RecipeSaver/Views/Main/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/10/23.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
