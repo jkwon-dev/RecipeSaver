d33e14185ffdaa7559b6b92e7b0cb3d723a66a9c
jkwon-dev <kwonejk@gmail.com>
2023-11-10 01:03:34 +0800

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
