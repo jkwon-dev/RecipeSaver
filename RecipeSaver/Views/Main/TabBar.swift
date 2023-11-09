e0f3b7bd843ebc3f9e987a8253930c078b9b1832
jkwon-dev <kwonejk@gmail.com>
2023-11-10 03:22:58 +0800

    Add TabBar

---
+++ RecipeSaver/Views/Main/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/10/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
