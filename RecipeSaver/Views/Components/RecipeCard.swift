6ce9449f818e7312dbf2d44e5866a51cf1e98c83
jkwon-dev <kwonejk@gmail.com>
2023-11-10 04:49:55 +0800

    create Recipe Card

---
+++ RecipeSaver/Views/Components/RecipeCard.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCard.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/10/23.
+//
+
+import SwiftUI
+
+struct RecipeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCard()
+}
