6a5338ce809fe5bea56d34bd10f0ae3c02bfcd5c
jkwon-dev <kwonejk@gmail.com>
2023-11-11 14:24:19 +0800

    RecipeView

---
+++ RecipeSaver/Views/Detail/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/11/23.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
