8681b28a78a50549a2d157874e3b9b43e6393f81
jkwon-dev <kwonejk@gmail.com>
2023-11-13 00:37:47 +0800

    submit form

--- RecipeSaver/Views/Detail/AddRecipeView.swift
+++ RecipeSaver/Views/Detail/AddRecipeView.swift
@@ -13,6 +13,7 @@ struct AddRecipeView: View {
     @State private var description: String = ""
     @State private var ingredients: String = ""
     @State private var directions: String = ""
+    @State private var navigateToRecipe = false
     
     @Environment(\.dismiss) var dismiss
     
@@ -51,14 +52,19 @@ struct AddRecipeView: View {
                     }
                 }
                 
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        
+                ToolbarItem() {
+                    NavigationLink(isActive: $navigateToRecipe) {
+                        RecipeView(recipe: Recipe.all.sorted{ $0.datePublished > $1.datePublished }[0])
+                            .navigationBarBackButtonHidden(true)
                     } label: {
-                        Label("Done", systemImage: "checkmark")
-                            .labelStyle(.iconOnly)
+                        Button {
+                            navigateToRecipe = true
+                        } label: {
+                            Label("Done", systemImage: "checkmark")
+                                .labelStyle(.iconOnly)
+                        }
+                        .disabled(name.isEmpty)
                     }
-                    .disabled(name.isEmpty)
                 }
             })
             .navigationTitle("New Recipe")
