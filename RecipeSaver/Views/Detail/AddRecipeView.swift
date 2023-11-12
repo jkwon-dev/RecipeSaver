5c5925f2cdf9cac0278970f87716e4531f0f865d
jkwon-dev <kwonejk@gmail.com>
2023-11-12 23:38:44 +0800

    Add Recipe Form - TextField, Picker

---
+++ RecipeSaver/Views/Detail/AddRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeView.swift
+//  RecipeSaver
+//
+//  Created by kwon eunji on 11/12/23.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeView()
+}
